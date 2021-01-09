a4d1dbd88f06537069448c986eaf8955d05c7788
gui_tar_gz <alexander.gruzdev@gmail.com>
2021-01-09 16:30:54 +0100

    add ifs

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print ("Hello World")
+# print ("Hello World")
+
+def cube(num):
+    return pow(num,3) #comment
+
+number = 3
+
+if number > 3:
+    result = cube(number)
+    print(result)
+else:
+    print("too small")
