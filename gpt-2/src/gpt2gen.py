696b2cc2d3cb323739dec555beaf8ed4888b74c4
gui_tar_gz <alexander.gruzdev@gmail.com>
2021-04-17 18:36:15 +0200

    fix for complete gpt-2 sentences

--- gpt-2/src/gpt2gen.py
+++ gpt-2/src/gpt2gen.py
@@ -6,6 +6,11 @@ def generate_caption(seed):
     generator = pipeline('text-generation', model='gpt2')
     set_seed(seed)
     caption = generator("My today Instagram beauty blogger post is: ", max_length=100, num_return_sequences=1)
+    caption = u''.join(caption[0]['generated_text'][42:])
+    for sep in '.!?':
+        caption = caption.rsplit(sep, 1)
+        if len(caption) > 1:
+            return caption[0]
     return caption
 
 def main():
@@ -13,7 +18,7 @@ def main():
     while True:
         files = open(file, 'wb')
         caption = generate_caption(random.randint(1, 999999999))
-        caption = u''.join(caption[0]['generated_text'][42:]).encode('utf-8', 'ignore')
+        caption = u''.join(caption).encode('utf-8', 'ignore')
         files.write(caption)
         files.close()
         time.sleep(21600)
